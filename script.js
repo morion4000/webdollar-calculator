6715e4c7118ad467b58f8ae3072dd960bf1e844c
Ionut Moraru <morion4000@gmail.com>
2019-04-08 11:36:36 +0300

    hybrid

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function compute() {
   var power = parseInt($('#power_input').val());
   var block_time = 40;
   var block_reward = 6000;
-  var dominance = power / global_power / 1000 * 100;
+  var dominance = power / global_power / 1000 * 100 / 3; // hybrid
   var reward_24 = block_reward * 60 * 60 * 24 / block_time * dominance / 100;
   var block_mining_time = 100 / dominance * block_time;
 
@@ -24,7 +24,12 @@ $(function() {
   compute();
 
   $.getJSON('https://server.webdollarminingpool.com', function(data) {
-    $('#global_power_input').val(parseInt(data.networkHashRate / 1000 / 1000));
+    var hashrate = parseInt(data.networkHashRate / 1000 / 1000); // in Mh/s
+
+    if (hashrate.length <= 5) {
+      $('#global_power_input').val(hashrate);
+    }
+
     compute();
   });
 });
